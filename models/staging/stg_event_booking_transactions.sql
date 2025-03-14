74b2e87c9a8ce57dca971abde4dc87d1587ac5af
Satheesh Periyasamy <satheesh.p@jmangroup.com>
2025-03-14 17:14:57 +0530

    added

--- models/staging/stg_event_booking_transactions.sql
+++ models/staging/stg_event_booking_transactions.sql
@@ -1,16 +1,16 @@
-
 WITH transactions_raw AS (
     SELECT * FROM {{ source('staging', 'transactions') }}
 ),
 
 cleaned_transactions AS (
     SELECT 
+        companies::STRING AS company,
         {{ cast('customer_id', 'STRING') }} AS customer_id,   
         {{ cast('product_id', 'STRING') }} AS product_id,
         TRY_CAST(payment_month AS DATE) AS payment_month, 
         COALESCE(revenue, 0)::FLOAT AS revenue,
-        COALESCE(quantity, 0)::INTEGER AS quantity,
-        companies::STRING AS company
+        COALESCE(revenue_type, 0)::INTEGER AS revenue_type,
+        COALESCE(quantity, 0)::INTEGER AS quantity
     FROM transactions_raw
     WHERE customer_id IS NOT NULL AND product_id IS NOT NULL
 )
