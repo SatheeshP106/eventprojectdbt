2e07de2fba7c40170c9de02e68528394fd166910
Satheesh Periyasamy <satheesh.p@jmangroup.com>
2025-03-14 18:34:40 +0530

    Updated revenue ranking

--- models/staging/stg_event_booking_customers.sql
+++ models/staging/stg_event_booking_customers.sql
@@ -1,19 +1,3 @@
--- WITH customers_raw AS (
---     SELECT * FROM {{ source('staging', 'customers') }}
--- ),
-
--- cleaned_customers AS (
---     SELECT 
---         TRIM(LOWER(company)) AS company,
---         customer_id::STRING AS customer_id,
---         COALESCE(TRIM(customername), 'Unknown') AS customer_name
---     FROM customers_raw
---     WHERE customer_id IS NOT NULL
--- )
-
--- SELECT * FROM cleaned_customers
-
-
 WITH customers_raw AS (
     SELECT * FROM {{ source('staging', 'customers') }}
 ),
